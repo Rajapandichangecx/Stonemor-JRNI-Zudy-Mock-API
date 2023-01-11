16afaee967e09a41dab43531a7798ec5725e75a2
Rajapandi <115097221+Rajapandichangecx@users.noreply.github.com>
2023-01-11 10:12:47 +0530

    contact route updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const ContactJSON = require("./json/contact.json");
 const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to Dummy API");
 });
+
+app.get("/contact", (req, res) => res.json(ContactJSON));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
